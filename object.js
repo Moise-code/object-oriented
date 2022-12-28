609b737ba6e5ccb3ec0cbeddbe1623cf45c1dc7a
Moise-code <moisewowe@gmail.com>
2022-12-28 09:32:51 +0200

    adding the object retuals using the class notation

--- object.js
+++ object.js
@@ -1,20 +1,35 @@
 // lets create a user object//
 
-const userOne = {
-    userName: 'ryu',
-    email: 'byiringiromoise12@gmail.com',
-    login(){
-        console.log('the user loogeed in successfully')
-    },
-    logout(){
-        console.log('the user logged out successfully');
-    }
-};
+// const userOne = {
+//     userName: 'ryu',
+//     email: 'byiringiromoise12@gmail.com',
+//     login(){
+//         console.log('the user loogeed in successfully')
+//     },
+//     logout(){
+//         console.log('the user logged out successfully');
+//     }
+// };
 
-console.log(userOne.email, userOne.userName);
-userOne.login();
+// console.log(userOne.email, userOne.userName);
+// userOne.login();
 // its okay when you want to create a new user object however it is properly and good to use the new keyword to create a new user object
 
-// example wee just use new keyword with the name user to create a new user
+// example wee just use new keyword with the name user to create a new user there is a way to use prototype model using the class keyword
+// a class is a bluue point of the object it demonstrate the properties and functionality using the blue print.
+// const userTwo = new User('amahoro@gmail.com','Moise');
 
-const userTwo = new User('amahoro@gmail.com','Moise');
+class User {
+    // create constructor to set the new user later on the constructor is containing the parameter of the argument chaning in the new user value
+    constructor(name, age){
+        // set up the properties here value of the property must be equals to the parameter passed in the constructor
+        this.username = name;
+        this.age = age;
+
+    }
+}
+// here in the new user bracket we are going to pass in the value in the constructor as the actual argument here in new user parameter.
+const UserOne = new User('Moise','60 years');
+const UserTwo = new User('Byiringiro', '70 years old');
+
+console.log(UserOne, UserTwo);
